04899e083d802e1e95102617002f99cfe96991f7
thekhoa2206@gmail.com <thekhoa2206@gmail.com>
2023-07-29 12:28:35 +0700

    commit

--- client/src/components/nav/Footer.js
+++ client/src/components/nav/Footer.js
@@ -1,15 +1,28 @@
-import { Box, Grid, Icon, Typography } from '@material-ui/core'
-import React from 'react'
+import { Box, Grid, Icon, Link, Typography } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
 import FacebookIcon from '../svg/FacebookIcon'
 import InstagramIcon from '../svg/InstagramIcon'
 import TwitterIcon from '../svg/TwitterIcon'
+import { getCategories } from '../../functions/category'
 
 const Footer = () => {
+  const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        getCategories().then((res) => setCategories(res.data));
+      }, []);
+
   return (
-    <Box style={{width: "100%", height: 150, background: "#e9e9e9", marginTop: 30}}>
+    <Box style={{width: "100%", minHeight: 150, background: "#e9e9e9", marginTop: 30}}>
       <Box style={{width: "60%", margin: "auto", padding: 10}}>
         <Grid xs={12} container>
-        <Grid xs={4} item></Grid>
+        <Grid xs={4} item>
+        <Typography style={{fontWeight: "bold", fontSize: 20}}>Categories</Typography>
+        {categories && categories.length && categories.map((item, index) => (
+        <Typography key={index} style={{textDecoration: "underline", cursor: "pointer"}} onClick={() => {window.location.href = `/category/${item.slug}`}}>
+          {item.name}
+          </Typography>
+        ))}
+        </Grid>
           <Grid xs={4} item> 
             <Typography style={{fontWeight: "bold", fontSize: 20}}>About us</Typography>
             <Typography style={{fontSize: 14}}>The store was opened in 2020 and has over 100,000 customers worldwide</Typography>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -108,10 +108,10 @@ const Home = () => {
       </h4>
       <BestSellers />
 
-      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+      {/* <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
         Categories
       </h4>
-      <CategoryList />
+      <CategoryList /> */}
 
       <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
         Sub Categories
